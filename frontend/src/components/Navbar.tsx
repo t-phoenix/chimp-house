257028e9bf0aedfec57b265e93b7c874a2d84f7b
Ayush Gupta <ayush.gupta@razorpay.com>
2023-06-30 15:47:10 +0530

    feat: add `framer-motion` for animations

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,25 @@
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import { Web3Button } from '@web3modal/react';
 import { Button } from './ui/button';
+import {
+  HERO_HEADING_DURATION,
+  HERO_HEADING_INITIAL_OPACITY,
+  HERO_HEADING_FINAL_OPACITY,
+} from './sections/HeroSection';
 
 export default function Navbar() {
   return (
-    <nav className='flex flex-row justify-between py-4 px-4 h-[10vh] text-primary bg-transparent'>
+    <motion.nav
+      animate={{ translateY: 0, opacity: HERO_HEADING_FINAL_OPACITY }}
+      initial={{ translateY: -100, opacity: HERO_HEADING_INITIAL_OPACITY }}
+      transition={{
+        ease: 'easeInOut',
+        duration: HERO_HEADING_DURATION,
+        delay: HERO_HEADING_DURATION,
+      }}
+      className='flex flex-row justify-between py-4 px-4 h-[10vh] text-primary bg-transparent'
+    >
       <Link to='/' className='group'>
         <div className='text-left text-base group-hover:text-accent'>
           Chimp House
@@ -24,6 +39,6 @@ export default function Navbar() {
       </div>
 
       <Web3Button />
-    </nav>
+    </motion.nav>
   );
 }
--- frontend/src/components/sections/ChimpHouseIntroSection.tsx
+++ frontend/src/components/sections/ChimpHouseIntroSection.tsx
@@ -1,16 +1,71 @@
-import { useState } from 'react';
-import { TypeAnimation } from 'react-type-animation';
+import { useEffect, useRef } from 'react';
+import { motion, useAnimate, stagger, useInView } from 'framer-motion';
+
+const staggerListItems = stagger(0.1, { startDelay: 0.15 });
+
+function useMenuAnimation(isInView: boolean) {
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    animate(
+      'ul',
+      {
+        clipPath: isInView
+          ? 'inset(0% 0% 0% 0% round 10px)'
+          : 'inset(10% 50% 90% 50% round 10px)',
+      },
+      {
+        type: 'spring',
+        bounce: 0,
+        duration: 0.5,
+      }
+    );
+
+    animate(
+      'li',
+      isInView
+        ? { opacity: 1, scale: 1, filter: 'blur(0px)' }
+        : { opacity: 0, scale: 0.3, filter: 'blur(20px)' },
+      {
+        duration: 0.3,
+        delay: isInView ? staggerListItems : 0,
+      }
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isInView]);
+
+  return scope;
+}
 
 function ChimpHouseIntroSection() {
-  const [typingAnimationTextColor, setTypingAnimationTextColor] = useState('');
+  const ref = useRef(null);
+  const isListInView = useInView(ref);
+
+  const scope = useMenuAnimation(isListInView);
 
   return (
-    <section className='min-h-screen flex flex-row justify-center gap-x-3 content-center bg-accent'>
-      <div className='text-9xl font-bold text-left w-1/2 place-self-center px-6'>
+    <section
+      ref={scope}
+      className='min-h-screen flex flex-col lg:flex-row justify-center gap-x-3 gap-y-16 content-center bg-accent'
+    >
+      <motion.div
+        initial={{ opacity: 0, translateX: -100 }}
+        whileInView={{ opacity: 1, translateX: 0 }}
+        transition={{ duration: 1 }}
+        className='text-6xl md:text-7xl lg:text-9xl font-bold text-left w-100 lg:w-1/2 place-self-start lg:place-self-center px-12'
+      >
         What is Chimp House?
-      </div>
-      <div className='w-1/2 flex flex-col justify-center content-center'>
-        <ul className='list-check-circle list-disc list-outside text-left'>
+      </motion.div>
+      <motion.div
+        ref={ref}
+        className='w-100 lg:w-1/2 flex flex-col justify-center content-center place-self-start lg:place-self-center px-12'
+      >
+        <ul
+          style={{
+            clipPath: 'inset(10% 50% 90% 50% round 10px)',
+          }}
+          className='list-check-circle list-disc list-outside text-left text-2xl font-medium leading-10'
+        >
           <li> NFT Themed Space</li>
           <li> Co-working Zones</li>
           <li> NFT Themed Space</li>
@@ -18,7 +73,7 @@ function ChimpHouseIntroSection() {
           <li> In-house Cafe</li>
           <li> Exclusive Destinations</li>
         </ul>
-      </div>
+      </motion.div>
     </section>
   );
 }
