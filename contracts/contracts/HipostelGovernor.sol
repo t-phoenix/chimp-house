93e4f3e58a296fa7cab0805b0708667f14424f9d
Abhinil Agarwal <abhinil@Abhinils-MacBook-Pro.local>
2023-07-02 03:18:40 +0530

    adding governance features

--- contracts/contracts/HipostelGovernor.sol
+++ contracts/contracts/HipostelGovernor.sol
@@ -6,17 +6,13 @@ import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
-import "@openzeppelin/contracts/governance/TimelockController.sol";
 
-
-contract HipostelGovernor is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction, GovernorTimelockControl {
-    constructor(IVotes _token, TimelockController _timelock)
+contract HipostelGovernor is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction {
+    constructor(IVotes _token)
         Governor("HipostelGovernor")
-        GovernorSettings(100 /* 20 minutes */, 300 /* 1 hour */, 2e18)
+        GovernorSettings(5 /* 1 minute */, 150 /* 30 minute */, 100e18)
         GovernorVotes(_token)
         GovernorVotesQuorumFraction(51)
-        GovernorTimelockControl(_timelock)
     {}
 
     // The following functions are overrides required by Solidity.
@@ -48,23 +44,6 @@ contract HipostelGovernor is Governor, GovernorSettings, GovernorCountingSimple,
         return super.quorum(blockNumber);
     }
 
-    function state(uint256 proposalId)
-        public
-        view
-        override(Governor, GovernorTimelockControl)
-        returns (ProposalState)
-    {
-        return super.state(proposalId);
-    }
-
-    function propose(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, string memory description)
-        public
-        override(Governor, IGovernor)
-        returns (uint256)
-    {
-        return super.propose(targets, values, calldatas, description);
-    }
-
     function proposalThreshold()
         public
         view
@@ -72,38 +51,5 @@ contract HipostelGovernor is Governor, GovernorSettings, GovernorCountingSimple,
         returns (uint256)
     {
         return super.proposalThreshold();
-    }
-
-    function _execute(uint256 proposalId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
-        internal
-        override(Governor, GovernorTimelockControl)
-    {
-        super._execute(proposalId, targets, values, calldatas, descriptionHash);
-    }
-
-    function _cancel(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
-        internal
-        override(Governor, GovernorTimelockControl)
-        returns (uint256)
-    {
-        return super._cancel(targets, values, calldatas, descriptionHash);
-    }
-
-    function _executor()
-        internal
-        view
-        override(Governor, GovernorTimelockControl)
-        returns (address)
-    {
-        return super._executor();
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(Governor, GovernorTimelockControl)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
     }
 }
--- contracts/contracts/TimelockContract.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
-
-import "@openzeppelin/contracts/governance/TimelockController.sol";
-
-contract TimelockContract is TimelockController {
-    constructor(uint256 delay, address[] memory proposers, address[] memory executors, address admin) TimelockController(delay, proposers, executors, admin) {}
-}
