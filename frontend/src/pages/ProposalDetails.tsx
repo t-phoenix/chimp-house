7f444d72ccb6a4cb99f81dbeef72a4663b0aa135
Ayush Gupta <ayush.gupta@razorpay.com>
2023-07-03 02:04:28 +0530

    refactor(ProposalDetails): beautify page

--- frontend/src/pages/ProposalDetails.tsx
+++ frontend/src/pages/ProposalDetails.tsx
@@ -1,10 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-nocheck
 
-import { ethers } from "ethers";
-import React from "react";
-import { useLocation } from "react-router-dom";
-import { useContractRead, useContractReads } from "wagmi";
-import { prepareWriteContract, readContract, writeContract } from "@wagmi/core";
+import { ethers } from 'ethers';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { useContractRead, useContractReads } from 'wagmi';
+import { prepareWriteContract, readContract, writeContract } from '@wagmi/core';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 // import { TokenABI } from "../ContractABIs/TokenABI";
 // import { GovernorABI } from "../ContractABIs/GovernorABI";
 import {
@@ -12,19 +16,87 @@ import {
   HIPtoken,
   GovernorABI,
   TokenABI,
-} from "../helper/contract";
-import { toETHdenomination } from "../helper/formatter";
+} from '../helper/contract';
+import { toETHdenomination } from '../helper/formatter';
+import CommonForm from '../components/shared/CommonForm';
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '../components/ui/hover-card';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '../components/ui/table';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../components/ui/select';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../components/ui/form';
+import { toast } from '../components/ui/use-toast';
+import { Button } from '../components/ui/button';
+
+const VoteSchema = z.object({
+  vote: z.union([z.literal('AGAINST'), z.literal('FOR'), z.literal('ABSTAIN')]),
+});
+
+const PROPOSAL_STATES = [
+  {
+    state: 0,
+    status: 'Pending',
+  },
+  {
+    state: 1,
+    status: 'Active',
+  },
+  {
+    state: 2,
+    status: 'Cancelled',
+  },
+  {
+    state: 3,
+    status: 'Defeated',
+  },
+  {
+    state: 4,
+    status: 'Succeded',
+  },
+  {
+    state: 6,
+    status: 'Expired',
+  },
+  {
+    state: 7,
+    status: 'Executed',
+  },
+];
 
 export default function ProposalDetails() {
   const { state } = useLocation();
-  console.log("State of APP:", state);
+  console.log('State of APP:', state);
 
   const tokenInterface = new ethers.utils.Interface(TokenABI);
   const decodeData = tokenInterface.decodeFunctionData(
-    "transfer",
+    'transfer',
     state.proposal.callDatas[0]
   );
-  console.log("Decoded Data:", decodeData);
+  console.log('Decoded Data:', decodeData);
 
   const governor = {
     address: HIPGovernor,
@@ -36,77 +108,105 @@ export default function ProposalDetails() {
       {
         address: HIPGovernor,
         abi: GovernorABI,
-        functionName: "proposalVotes",
+        functionName: 'proposalVotes',
         args: [state.proposal.proposalId],
       },
       {
         address: state.proposal.targets[0],
         abi: TokenABI,
-        functionName: "symbol",
+        functionName: 'symbol',
       },
     ],
   });
 
-  console.log("Proposal Votes:", data);
+  console.log('Proposal Votes:', data);
+  const voteForm = useForm<z.infer<typeof VoteSchema>>({
+    resolver: zodResolver(VoteSchema),
+  });
 
   async function handleVote(votePref) {
     if (votePref == 0) {
-      console.log("Vote FOR Proposal");
+      console.log('Vote FOR Proposal');
       const config = await prepareWriteContract({
         ...governor,
-        functionName: "castVote",
+        functionName: 'castVote',
         args: [state.proposal.proposalId, 0],
       });
 
       const data = await writeContract(config);
-      console.log("After Granting Role:", data);
+      console.log('After Granting Role:', data);
     } else if (votePref == 1) {
-      console.log("Vote against proposal");
+      console.log('Vote against proposal');
       const config = await prepareWriteContract({
         ...governor,
-        functionName: "castVote",
+        functionName: 'castVote',
         args: [state.proposal.proposalId, 1],
       });
 
       const data = await writeContract(config);
-      console.log("After Granting Role:", data);
+      console.log('After Granting Role:', data);
     } else if (votePref == 2) {
-      console.log("Vote abstain proposal");
+      console.log('Vote abstain proposal');
       const config = await prepareWriteContract({
         ...governor,
-        functionName: "castVote",
+        functionName: 'castVote',
         args: [state.proposal.proposalId, 1],
       });
 
       const data = await writeContract(config);
-      console.log("After Granting Role:", data);
+      console.log('After Granting Role:', data);
     }
   }
 
-//   async function queueProposal() {
-//     const descriptionHash = ethers.utils.id(state.proposal.description);
-//     console.log("PROP QUEUE:", state.proposal.description, descriptionHash);
+  function onVoteSubmit(data: z.infer<typeof VoteSchema>) {
+    console.log(data);
 
-//     const config = await prepareWriteContract({
-//       ...governor,
-//       functionName: "queue",
-//       args: [
-//         state.proposal.targets,
-//         state.proposal.values,
-//         state.proposal.callDatas,
-//         descriptionHash,
-//       ],
-//     });
+    switch (data.vote) {
+      case 'AGAINST':
+        handleVote(0);
+        break;
+      case 'FOR':
+        handleVote(1);
+        break;
+      case 'ABSTAIN':
+        handleVote(2);
+        break;
+    }
 
-//     const data = await writeContract(config);
-//     console.log("After Queuing Proposal:", data);
-//   }
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
+          <code className='text-white'>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  //   async function queueProposal() {
+  //     const descriptionHash = ethers.utils.id(state.proposal.description);
+  //     console.log("PROP QUEUE:", state.proposal.description, descriptionHash);
+
+  //     const config = await prepareWriteContract({
+  //       ...governor,
+  //       functionName: "queue",
+  //       args: [
+  //         state.proposal.targets,
+  //         state.proposal.values,
+  //         state.proposal.callDatas,
+  //         descriptionHash,
+  //       ],
+  //     });
+
+  //     const data = await writeContract(config);
+  //     console.log("After Queuing Proposal:", data);
+  //   }
 
   async function executeProposal() {
     const descriptionHash = ethers.utils.id(state.proposal.description);
     const config = await prepareWriteContract({
       ...governor,
-      functionName: "execute",
+      functionName: 'execute',
       args: [
         state.proposal.targets,
         state.proposal.values,
@@ -116,139 +216,235 @@ export default function ProposalDetails() {
     });
 
     const data = await writeContract(config);
-    console.log("After Executing Proposal:", data);
+    console.log('After Executing Proposal:', data);
   }
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", color: "white" }}>
-      <h3>DAO Proposal Details</h3>
-      <p>Description: {state.proposal.description}</p>
-      <p>Proposal State: {state.proposal.proposalState}</p>
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "start",
-            padding: "10px",
-          }}
-        >
-          <h4>Proposal States</h4>
-          <p>0 Pending</p>
-          <p>1 Active</p>
-          <p>2 Cancelled</p>
-          <p>3 Defeated</p>
-          <p>4 Succeded</p>
-          <p>6 Expired</p>
-          <p>7 Executed </p>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "start",
-            marginLeft: "40px",
-            padding: "10px",
-          }}
-        >
-          <p>Governor Contract: {HIPGovernor}</p>
-          <p>Proposer's Address: {state.proposal.proposer}</p>
-          <p>Proposal Id: {state.proposal.proposalId}</p>
-          <p>Transaction Hash: {state.proposal.transactionHash}</p>
-          <p>Voting Start (block): {state.proposal.votingStartDate}</p>
-          <p>Voting End (block): {state.proposal.votingEndDate}</p>
-          {data && (
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                flexWrap: "wrap",
-                justifyContent: "space-around",
-                width: "70%",
-              }}
-            >
-              <p>Votes For: {toETHdenomination(Number(data[0].forVotes))}</p>
-              <p>
-                Votes Against: {toETHdenomination(Number(data[0].againstVotes))}
-              </p>
-              <p>
-                Votes Abstain: {toETHdenomination(Number(data[0].abstainVotes))}
-              </p>
-            </div>
-          )}
+    <section className='flex flex-col text-white'>
+      <h1 className='text-4xl font-extrabold tracking-tight lg:text-6xl text-accent drop-shadow-sm py-10'>
+        DAO Manager
+      </h1>
+      <h3 className='pb-4'>Description</h3>
 
-          <p>Decoded Data: (for simple proposal)</p>
-          <p>Amount Receiver: {decodeData.to}</p>
-          {data && (
-            <p>
-              Amount: {Number(decodeData.amount)} {data[1]}
-            </p>
-          )}
-        </div>
-      </div>
-      {state.proposal.proposalState == 1 ? (
-        <div>
-          <div
-            style={{
-              marginBlock: "2%",
-              width: "90%",
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-around",
-            }}
-          >
-            <button onClick={() => handleVote(0)}>Against</button>
-            <button onClick={() => handleVote(1)}>For</button>
-            <button onClick={() => handleVote(2)}>Abstain</button>
+      <CommonForm legendTitle='Proposal Details'>
+        <div className='overflow-scroll flex flex-col flex-wrap flex-grow justify-between gap-4 lg:gap-8'>
+          <div className='flex flex-col'>
+            <div className='text-left text-sm uppercase font-medium text-gray-200'>
+              Proposal Id
+            </div>
+            <div className='text-left text-2xl font-bold text-gray-50'>
+              {state.proposal.proposalId}
+            </div>
           </div>
-          <p style={{ fontSize: "12px" }}>
-            Check your voting power on token Screen before voting.
-          </p>
+
+          <div className='flex flex-col justify-start items-start'>
+            <div className='text-left text-sm uppercase font-medium text-gray-200'>
+              Description
+            </div>
+            <div className='text-left text-2xl font-bold text-gray-50'>
+              {state.proposal.description}
+            </div>
+          </div>
+
+          <div className='flex flex-col'>
+            <div className='text-left text-sm uppercase font-medium text-gray-200'>
+              Proposal State
+            </div>
+            <div className='text-left text-2xl font-bold text-gray-50'>
+              {state.proposal.proposalState}
+            </div>
+          </div>
+
+          <div className='flex flex-col'>
+            <div className='text-left text-sm uppercase font-medium text-gray-200'>
+              Governor Contract
+            </div>
+            <div className='text-left text-2xl font-bold text-gray-50'>
+              {HIPGovernor}
+            </div>
+          </div>
+
+          <div className='flex flex-col'>
+            <div className='text-left text-sm uppercase font-medium text-gray-200'>
+              Proposer's Address
+            </div>
+            <div className='text-left text-2xl font-bold text-gray-50'>
+              {state.proposal.proposer}
+            </div>
+          </div>
+
+          <div className='flex flex-col'>
+            <div className='text-left text-sm uppercase font-medium text-gray-200'>
+              Transaction Hash
+            </div>
+            <div className='text-left text-2xl font-bold text-gray-50'>
+              {state.proposal.transactionHash}
+            </div>
+          </div>
+
+          <div className='flex flex-col'>
+            <div className='text-left text-sm uppercase font-medium text-gray-200'>
+              Voting Start (block)
+            </div>
+            <div className='text-left text-2xl font-bold text-gray-50'>
+              {state.proposal.votingStartDate}
+            </div>
+          </div>
+
+          <div className='flex flex-col'>
+            <div className='text-left text-sm uppercase font-medium text-gray-200'>
+              Voting End (block)
+            </div>
+            <div className='text-left text-2xl font-bold text-gray-50'>
+              {state.proposal.votingEndDate}
+            </div>
+          </div>
+
+          {data ? (
+            <div className='flex flex-wrap flex-row space-x-4'>
+              <div className='flex flex-col'>
+                <div className='text-left text-sm uppercase font-medium text-gray-200'>
+                  Votes For
+                </div>
+                <div className='text-left text-2xl font-bold text-gray-50'>
+                  {toETHdenomination(Number(data[0].forVotes))}
+                </div>
+              </div>
+              <div className='flex flex-col'>
+                <div className='text-left text-sm uppercase font-medium text-gray-200'>
+                  Votes Against
+                </div>
+                <div className='text-left text-2xl font-bold text-gray-50'>
+                  {toETHdenomination(Number(data[0].againstVotes))}
+                </div>
+              </div>
+              <div className='flex flex-col'>
+                <div className='text-left text-sm uppercase font-medium text-gray-200'>
+                  Votes Abstain
+                </div>
+                <div className='text-left text-2xl font-bold text-gray-50'>
+                  {toETHdenomination(Number(data[0].abstainVotes))}
+                </div>
+              </div>
+            </div>
+          ) : null}
+
+          <div className='flex flex-col'>
+            <div className='text-left text-sm uppercase font-medium text-gray-200'>
+              Amount Receiver
+            </div>
+            <div className='text-left text-2xl font-bold text-gray-50'>
+              {decodeData.to}
+            </div>
+            d
+          </div>
+
+          {data ? (
+            <div className='flex flex-col'>
+              <div className='text-left text-sm uppercase font-medium text-gray-200'>
+                Amount
+              </div>
+              <div className='text-left text-2xl font-bold text-gray-50'>
+                {Number(decodeData.amount)} {data[1]}
+              </div>
+            </div>
+          ) : null}
         </div>
-      ) : (
-        <div></div>
-      )}
+      </CommonForm>
+
+      {state.proposal.proposalState == 1 ? (
+        <Form {...voteForm}>
+          <form
+            onSubmit={voteForm.handleSubmit(onVoteSubmit)}
+            className='w-2/3 space-y-6'
+          >
+            <FormField
+              control={voteForm.control}
+              name='vote'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Vote</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder='Select your vote' />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value='AGAINST'>Against</SelectItem>
+                      <SelectItem value='FOR'>For</SelectItem>
+                      <SelectItem value='ABSTAIN'>Abstain</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormDescription>
+                    Check your voting power on token Screen before voting.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type='submit'>Vote</Button>
+          </form>
+        </Form>
+      ) : null}
+
       {state.proposal.proposalState == 2 ? (
         <div>
           <h3>Proposal Cancelled</h3>
         </div>
-      ) : (
-        <div></div>
-      )}
+      ) : null}
+
       {state.proposal.proposalState == 3 ? (
         <div>
           <h3>Proposal Defeated</h3>
         </div>
-      ) : (
-        <div></div>
-      )}
+      ) : null}
+
       {state.proposal.proposalState == 4 ? (
         <div>
           <p>Proposal Succeded</p>
-          <button
-            style={{ marginTop: "15px", marginBottom: "25px" }}
-            onClick={executeProposal}
-          >
+          <Button className='m-8' onClick={executeProposal}>
             Execute
-          </button>
+          </Button>
         </div>
-      ) : (
-        <div></div>
-      )}
+      ) : null}
+
       {state.proposal.proposalState == 6 ? (
         <div>
           <p>Proposal Expired</p>
         </div>
-      ) : (
-        <div></div>
-      )}
+      ) : null}
+
       {state.proposal.proposalState == 7 ? (
         <div>
           <h3>Proposal Executed</h3>
         </div>
-      ) : (
-        <div></div>
-      )}
-    </div>
+      ) : null}
+
+      <CommonForm legendTitle='Proposal State Mapping'>
+        <Table>
+          <TableCaption>A mapping of proposal states.</TableCaption>
+          <TableHeader>
+            <TableRow className='text-left'>
+              <TableHead>Proposal State</TableHead>
+              <TableHead>Status</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody className='text-left'>
+            {PROPOSAL_STATES.map((proposalState, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell>{proposalState.state}</TableCell>
+                  <TableCell>{proposalState.status}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </CommonForm>
+    </section>
   );
 }
